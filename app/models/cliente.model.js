19f944fc11796110d487900784374039144aa6e1
Roberto <sheypol132@gmail.com>
2025-03-27 17:03:32 -0600

    apicliente

---
+++ app/models/cliente.model.js
@@ -0,0 +1,46 @@
+module.exports = (sequelize, Sequelize) => {
+    const Clientes = sequelize.define("CLIENTES", {
+      ID_CLIENTE: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: false
+      },
+      NOMBRES: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      APELLIDOS: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      FECHA_NACIMIENTO: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      NIT: {
+        type: Sequelize.STRING(20)
+      },
+      DIRECCION: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
+      CORREO_ELECTRONICO: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      USUARIO: {
+        type: Sequelize.STRING(30),
+        allowNull: false
+      },
+      CONTRASENA: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      }
+    }, {
+      tableName: "CLIENTES",
+      timestamps: false,
+      schema: "C##HYPER"  // Especifica el esquema de Oracle
+    });
+  
+    return Clientes;
+  }
