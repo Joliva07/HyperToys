c83b85f414370af18fb8d8ec4aeb327255315e36
Horacio Lopez <lopezjosecbsl@gmail.com>
2025-05-21 01:26:44 -0600

    webhook-FINAL

--- Back-end/app/controllers/pago.controller.js
+++ Back-end/app/controllers/pago.controller.js
@@ -34,7 +34,7 @@ exports.createPaymentIntent = async (req, res) => {
       })),
       mode: 'payment',
       success_url: `${env.FRONTEND_URL}/#/pago-exitoso?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${env.FRONTEND_URL}/confirmar-compra`,
+      cancel_url: `${env.FRONTEND_URL}/#/confirmar-compra`,
       metadata: {
         id_cliente: ID_CLIENTE,
         productos: JSON.stringify(productosMinimos),
